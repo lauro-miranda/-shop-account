a4c4f5b52ca92b5df39bf8ff98efdcd68aed716e
Lauro Miranda <lauro.miranda@woozaventures.com.br>
2021-05-20 22:42:51 -0300

    /me

---
+++ ToSoftware.Shop.Accounts.Api/Controllers/Default/MeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToSoftware.Shop.Accounts.Api.Controllers.Default
+{
+    [ApiController, Route("")]
+    public class MeController : ControllerBase
+    {
+        [HttpGet, Route("")]
+        public IActionResult Get() => Ok(new { name = "shop account", version = "1.0" });
+    }
+}
